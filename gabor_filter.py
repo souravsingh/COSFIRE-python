aa58791834f58b217c83cc3396720c2e0c93ff61
Sourav Singh <souravsingh@users.noreply.github.com>
2017-03-10 21:37:52 +0530

    Update gabor_filter.py

--- gabor_filter.py
+++ gabor_filter.py
@@ -1,12 +1,8 @@
-import numpy as np
-
 from skimage import io as io
 from skimage.filters import gabor
 from skimage import data
 
 from matplotlib import pyplot as plt 
-
-np.random.seed(42)
 
 image = data.coins()
 
@@ -15,4 +11,4 @@ filt_real, filt_imag = gabor(image, frequency=0.6)
 plt.figure()
 
 io.imshow(filt_real)
-io.show(0
+io.show()

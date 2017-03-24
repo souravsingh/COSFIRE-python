cbeeb5490dba4bf851b8f9095e4123d1fd74aff2
Sourav Singh <souravsingh@users.noreply.github.com>
2017-03-24 21:04:47 +0530

    Update gabor_filter.py

--- gabor_filter.py
+++ gabor_filter.py
@@ -1,14 +1,23 @@
 from skimage import io as io
 from skimage.filters import gabor
 from skimage import data
+from skimage.util import img_as_float
+
+from scipy import ndimage as ndi
+
+import numpy as np
 
 from matplotlib import pyplot as plt 
 
-image = data.coins()
+def build_filters():
+    filters = []
+    ksize = 9
+    for theta in range(0, 8):
+        theta = theta / 4. * np.pi
+        for lamda in np.arange(0, np.pi, np.pi/4): 
+            kern = cv2.getGaborKernel((ksize, ksize), 1.0, theta, lamda, 0.5, 0, ktype=cv2.CV_32F)
+            kern /= 1.5*kern.sum()
+            filters.append(kern)
+    return filters
 
-filt_real, filt_imag = gabor(image, frequency=0.6)
 
-plt.figure()
-
-io.imshow(filt_real)
-io.show()
